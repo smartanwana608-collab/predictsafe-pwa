be9d94c1ebfc86e48eb768efe781a5351931d6c7
smartanwana608-collab <smartanwana608@gmail.com>
2026-03-31 09:05:34 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,22 @@
-self.addEventListener("install", function () {
-    console.log("App installed");
+const CACHE_NAME = "predictsafe-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
